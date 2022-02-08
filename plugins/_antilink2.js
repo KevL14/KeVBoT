cbd407b8d2fa0327b1e30ea97d7bf9544a644189
KevL14 <99137212+KevL14@users.noreply.github.com>
2022-02-08 01:02:19 -0600

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -12,12 +12,12 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
     await m.reply(`*3!!*`)
     await m.reply(`*2!!*`)
     await m.reply(`*1!!*`)
-    if (isAdmin) return m.reply('*Te salvaste cagon(a) eres admin, no puedo eliminarte :v*')
+    if (isAdmin) return m.reply('*Te salvaste, eres admin, no puedo eliminarte :v*')
     if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
     let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
     let isLinkThisGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkThisGc.test(m.text)
-    if (isgclink) return m.reply('*Lol.. enviaste el enlace de este grupo :v*')
+    if (isgclink) return m.reply('*enviaste el enlace de este grupo :v*')
     await this.groupRemove(m.chat, [m.sender])
   }
   return true
